75ac05205eed824dc02069e129956c79688a8469
Utpal Rajbongshi <127889312+utpalr533@users.noreply.github.com>
2025-10-09 13:37:39 +0000

    SImple dictionary program of 5 words in js

---
+++ 01/dictionary.js
@@ -0,0 +1,8 @@
+const dictionary = {
+    apple: "A fruit that is usually red, green, or yellow.",
+    banana: "A long, curved fruit that is yellow when ripe.",
+    cat: "A small domesticated carnivorous mammal with soft fur.",
+    dog: "A domesticated carnivorous mammal that typically has a long snout.",
+    elephant: "A large herbivorous mammal with a trunk, native to Africa and Asia."
+}
+console.log(dictionary.apple) // Output: A fruit that is usually red, green, or yellow.
