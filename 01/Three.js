5435eef37ebf3e1275476bb80396445a6d4e606c
Utpal Rajbongshi <127889312+utpalr533@users.noreply.github.com>
2025-10-09 13:35:44 +0000

    creating an object and adding a new key to the object

---
+++ 01/Three.js
@@ -0,0 +1,19 @@
+// create a const object in javascript. Can you change it to hold a number later?
+
+const superHeroes = {
+    Marvel: "Iron-Man",
+    DC: "Batman",
+    Manga: "Kenshin"
+    // Output: Error;
+} 
+
+// CHaning const to hold a number
+superHeroes.Marvel = "42"
+
+// Adding a new key-value pair to the object
+superHeroes.anime = "Kakasi"
+console.log(superHeroes) // Output;
+
+console.log(superHeroes.Manga) 
+
+
