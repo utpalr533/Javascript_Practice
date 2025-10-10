4cfc3f63b4e593f7f3025a1684feee6ef7476abf
Utpal Rajbongshi <127889312+utpalr533@users.noreply.github.com>
2025-10-10 05:11:13 +0000

    a simple dictionary

--- 01/dictionary.js
+++ 01/dictionary.js
@@ -6,3 +6,16 @@ const dictionary = {
     elephant: "A large herbivorous mammal with a trunk, native to Africa and Asia."
 }
 console.log(dictionary.apple) // Output: A fruit that is usually red, green, or yellow.
+
+const dictionary2 = {
+    apple: "A fruit that is usually red, green, or yellow.",
+    banana: "A long, curved fruit that is yellow when ripe.",
+    cat: "A small domesticated carnivorous mammal with soft fur.",
+    dog: "A domesticated carnivorous mammal that typically has a long snout.",
+    elephant: "A large herbivorous mammal with a trunk, native to Africa and Asia."
+}
+   
+
+console.log(dictionary.apple)
+console.log(dictionary2.apple);
+ // Output: A fruit that is usually red, green, or yellow.
