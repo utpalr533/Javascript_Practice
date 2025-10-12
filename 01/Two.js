de7e9779dce2520da091f1391e73945ee9ae09a1
Utpal Rajbongshi <127889312+utpalr533@users.noreply.github.com>
2025-10-12 14:15:11 +0000

    String manupulation / concat method

--- 01/Two.js
+++ 01/Two.js
@@ -3,3 +3,17 @@
 const Name = "Utpal"
 console.log(Name + 5) // Output: Utpal5
 console.log(typeof(Name + 5)) // Output: string;
+  
+
+
+let str = "Hello";
+let num = 10;
+let result = str + num; // Concatenation of string and number
+console.log(result); // Output: Hello10
+console.log(typeof(result)); // Output: string
+
+let str2 = "The answer is ";
+let num2 = 42;
+let result2 =  str2.concat(" ", num2)
+console.log(result2); // Output: The answer is 42
+console.log(typeof(result2)); // Output: string
