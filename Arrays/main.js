31198fa3f00a7596d20f18f3de7726322472ecb3
Utpal Rajbongshi <127889312+utpalr533@users.noreply.github.com>
2025-10-20 05:07:49 +0000

    Arrays and object

---
+++ Arrays/main.js
@@ -0,0 +1,32 @@
+let fruits = ["Apple", "Banana", "Mango"];
+console.log(fruits);
+fruits[1]= "Orange";
+console.log(fruits);
+fruits.push("Pineapple");
+console.log(fruits);
+fruits.pop();
+console.log(fruits);
+for (let i = 0; i < fruits.length; i++) {
+    console.log(fruits[i]);
+}
+
+
+let person ={
+    name: "John",
+    age: 30,
+    city: "New York",
+    isStudent: false,
+};
+console.log(person.name);
+console.log(person ['age']);
+
+person.age = 31;
+console.log(person.age);
+
+person.city = "Los Angeles";
+console.log(person);
+
+for (let key in person ){
+    console.log(key +': ' +person[key]);
+    
+}
